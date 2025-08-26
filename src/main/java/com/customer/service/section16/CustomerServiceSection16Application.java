857d70df35ae03584593767b0f0ce7006d2e4d47
HayathullaKhan48 <nayazihayathulla48@gmail.com>
2025-08-26 15:38:53 +0530

    initial commit

---
+++ src/main/java/com/customer/service/section16/CustomerServiceSection16Application.java
@@ -0,0 +1,13 @@
+package com.customer.service.section16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceSection16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceSection16Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/customer/service/section16/CustomerServiceSection16ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customer.service.section16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceSection16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
